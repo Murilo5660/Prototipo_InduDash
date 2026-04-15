b7d33ff8175d3a9ac4146b65540f4c88b9773cd2
Murilo Ferreira <murilo.stresser@escola.pr.gov.br>
2026-04-14 21:41:45 -0300

    Add files via upload

---
+++ Meu_Dashboard/script.js
@@ -0,0 +1,13 @@
+const allSideMenu = document.querySelectorAll('#barra_lateral .side-menu.top li a');
+
+allSideMenu.forEach(item => {
+    const li = item.parentElement;
+
+    item.addEventListener('click', function () {
+        allSideMenu.forEach(i => {
+            i.parentElement.classList.remove('active');
+        });
+
+        li.classList.add('active');
+    });
+});
